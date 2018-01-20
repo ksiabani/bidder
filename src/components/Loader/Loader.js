368fad859a61f71254a04ddbcc6ffa5d482e0c20
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-01-20 23:56:34 +0200

    Updates

--- src/components/Loader/Loader.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Loader = () => (
-    <span className="loader"></span>
-);
-
-export default Loader;
--- src/constants.js
+++
@@ -1,1 +0,0 @@
-export const API = 'https://private-anon-57b3da0554-biddermanagement.apiary-mock.com/bidders';
