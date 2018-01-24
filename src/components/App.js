a7fedca85353a4c01ce67e58b3864f64912c0eec
Kostas Siabanis <kostas.siabanis@gmail.com>
2018-01-24 18:59:47 +0200

    Add PrivateRoute component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Wrapper from './Wrapper/Wrapper';
 import Login from './Login/Login';
+import PrivateRoute from './PrivateRoute';
 
 const App = () => (
     <Switch>
         {/*<Wrapper />*/}
         <Route exact path="/login" component={Login} />
-        <Route path="/" component={Wrapper} />
+        <PrivateRoute path="/" component={Wrapper} />
     </Switch>
 );
 
